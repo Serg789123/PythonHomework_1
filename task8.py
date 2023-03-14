e0160aa55c9bad602d5e28d5001016f94fd7d226
Sergey <diuk@inbox.ru>
2023-03-14 22:29:15 +0300

    исправление 8 задачи

--- task8.py
+++ task8.py
@@ -8,7 +8,7 @@
 n = int(input("Введите число n: "))
 m = int(input("Введите число m: "))
 k = int(input("Введите число k: "))
-if k % n == 0 or k % m == 0:
+if (k % n == 0 or k % m == 0) and k <= n*m:
     print('Yes')
 else:
     print('No')   
